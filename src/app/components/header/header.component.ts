865f51c4b20d4570262e4393d89c24ceb5f20341
Lukasz Obuchowicz <lobuchowicz@remot.eee>
2021-08-16 16:01:09 +0200

    fixed header wip

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnDestroy,
+} from '@angular/core';
+import { WINDOW } from '@ng-web-apis/common';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
+export class HeaderComponent implements AfterViewInit, OnDestroy {
+  private scrollSub?: Subscription;
 
-  ngOnInit(): void {}
+  constructor(
+    private headerRef: ElementRef,
+    @Inject(DOCUMENT) readonly documentRef: Document,
+    @Inject(WINDOW) readonly windowRef: Window
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.scrollSub = fromEvent(this.documentRef, 'scroll').subscribe((res) =>
+      console.log(res)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.scrollSub?.unsubscribe();
+  }
 }
